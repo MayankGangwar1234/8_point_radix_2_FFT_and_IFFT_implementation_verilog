bd5b88d93b4e3e32ebdbe4202570289a74d8d746
SHUDHANSHU-BHADANA <shudhanshubhadana1234@gmail.com>
2025-08-09 22:57:09 +0530

    Update bit_reversal_8point.v

--- bit_reversal_8point.v
+++ bit_reversal_8point.v
@@ -1,20 +1,23 @@
 module bit_reversal_8point (
-    input wire [15:0] real_in [0:7], // Real part of inputs
-    input wire [15:0] imag_in [0:7], // Imaginary part of inputs
-    output reg [15:0] real_out [0:7], // Real part of reordered outputs
-    output reg [15:0] imag_out [0:7]  // Imaginary part of reordered outputs
+  input  [15:0] real_in0, real_in1, real_in2, real_in3,
+  input  [15:0] real_in4, real_in5, real_in6, real_in7,
+  input  [15:0] imag_in0, imag_in1, imag_in2, imag_in3,
+  input  [15:0] imag_in4, imag_in5, imag_in6, imag_in7,
+  output [15:0] real_out0, real_out1, real_out2, real_out3,
+  output [15:0] real_out4, real_out5, real_out6, real_out7,
+  output [15:0] imag_out0, imag_out1, imag_out2, imag_out3,
+  output [15:0] imag_out4, imag_out5, imag_out6, imag_out7
 );
 
-    // Bit-reversed index mapping
-  always @(*) begin
-        real_out[0] = real_in[0]; imag_out[0] = imag_in[0]; // 000 -> 000
-        real_out[1] = real_in[4]; imag_out[1] = imag_in[4]; // 001 -> 100
-        real_out[2] = real_in[2]; imag_out[2] = imag_in[2]; // 010 -> 010
-        real_out[3] = real_in[6]; imag_out[3] = imag_in[6]; // 011 -> 110
-        real_out[4] = real_in[1]; imag_out[4] = imag_in[1]; // 100 -> 001
-        real_out[5] = real_in[5]; imag_out[5] = imag_in[5]; // 101 -> 101
-        real_out[6] = real_in[3]; imag_out[6] = imag_in[3]; // 110 -> 011
-        real_out[7] = real_in[7]; imag_out[7] = imag_in[7]; // 111 -> 111
-    end
+  // Continuous assignments for bit reversal
+  assign real_out0 = real_in0; assign imag_out0 = imag_in0;
+  assign real_out1 = real_in4; assign imag_out1 = imag_in4;
+  assign real_out2 = real_in2; assign imag_out2 = imag_in2;
+  assign real_out3 = real_in6; assign imag_out3 = imag_in6;
+  assign real_out4 = real_in1; assign imag_out4 = imag_in1;
+  assign real_out5 = real_in5; assign imag_out5 = imag_in5;
+  assign real_out6 = real_in3; assign imag_out6 = imag_in3;
+  assign real_out7 = real_in7; assign imag_out7 = imag_in7;
 
 endmodule
+
